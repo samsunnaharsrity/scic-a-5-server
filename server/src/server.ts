5a51791293dfe380170b1dafca028ee6e164a4a9
Samsunnahar Srity <samsunnaharsrity@gmail.com>
2026-07-20 09:49:45 +0600

    server up

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,62 +1,19 @@
 import dotenv from "dotenv";
 import serverless from "serverless-http";
-
 import app from "./app";
 import { connectDB } from "./config/mongodb";
 
-
 dotenv.config();
-
 
 let connected = false;
 
-
 const handler = serverless(app);
 
-
-export default async function (
-  req: any,
-  res: any
-) {
-
-  try {
-
-    if (!connected) {
-
-      await connectDB();
-
-      connected = true;
-
-      console.log("✅ MongoDB Connected");
-
-    }
-
-
-    return handler(req, res);
-
-
-  } catch (error) {
-
-
-    console.error(
-      "❌ Serverless Error:",
-      error
-    );
-
-
-    return res.status(500).json({
-
-      success:false,
-
-      message:"Server Error",
-
-      error:
-      error instanceof Error
-      ? error.message
-      : error
-
-    });
-
+export default async function (req: any, res: any) {
+  if (!connected) {
+    await connectDB();
+    connected = true;
   }
 
+  return handler(req, res);
 }
